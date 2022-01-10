59828fc95f32aabd2f9f52dd820cb6f91747224c
Wendy-bear <w6363980@gmail.com>
2022-01-10 20:07:24 +0300

    +

--- src/components/chatapp.jsx
+++ src/components/chatapp.jsx
@@ -1,27 +1,44 @@
 import "./chatapp.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Chat() {
-  let [send, showSend] = useState("");
+  let [messages, addMessage] = useState([]);
+  let [text, setText] = useState("");
+
+  //не понимаю, как зафиксировать дату для каждого рендеринга по клику
 
   let data = new Date();
-  console.log(data);
-  let newdata = data.toLocaleString();
-  console.log(newdata);
 
-  //  function messageSend() {
-  //    return <div className="message">{send}</div>;
-  //  }
+  let newdata = data.toLocaleString();
+
+  let messageList = messages
+    .slice(0)
+    .reverse()
+    .map((message, index) => {
+      return (
+        <div key={index}>
+          {newdata} : {message}
+        </div>
+      );
+    });
+
+  console.log(messages);
+
+  function addMessages() {
+    addMessage([...messages, text]);
+  }
 
   return (
     <div className="wrapper">
       <input
         placeholder="введите сообщение"
-        value={send}
-        onChange={(event) => showSend(event.target.value)}
+        value={text}
+        onChange={(event) => setText(event.target.value)}
       />
-      <button className="btn-send">Отправить</button>
-      <div className="message">{newdata + ":  " + send}</div>
+      <button className="btn-send" onClick={addMessages}>
+        Отправить
+      </button>
+      <div className="message">{messageList}</div>
     </div>
   );
 }
